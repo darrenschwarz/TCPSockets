ad09e8f1265eea84179014e0dacae06d887d35ec
darrenschwarz <darrenschwarz@gmail.com>
2015-12-15 22:01:38 +0000

    Initial commit, Asynchronous & Synchronous TCP sockets examples

---
+++ src/SynchronousServerSocket/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SynchronousServerSocket
+{
+    public class SynchronousSocketListener
+    {
+
+        public static void DoGetHostEntry(string hostname)
+        {
+            IPHostEntry host;
+
+            host = Dns.GetHostEntry(hostname);
+
+            Console.WriteLine("GetHostEntry({0}) returns:", hostname);
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                Console.WriteLine("    {0}", ip);
+            }
+        }
+
+        // Incoming data from the client.
+        public static string data = null;
+
+        public static void StartListening()
+        {
+            // Data buffer for incoming data.
+            var bytes = new Byte[1024];
+
+            // Establish the local endpoint for the socket.
+            // Dns.GetHostName returns the name of the 
+            // host running the application.
+            DoGetHostEntry(Dns.GetHostName());
+
+            var ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());//.Resolve(Dns.GetHostName());
+            var ipAddress = ipHostInfo.AddressList[2];
+            var localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP socket.
+            var listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and 
+            // listen for incoming connections.
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                // Start listening for connections.
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    // Program is suspended while waiting for an incoming connection.
+                    var handler = listener.Accept();
+                    data = null;
+
+                    // An incoming connection needs to be processed.
+                    while (true)
+                    {
+                        bytes = new byte[1024];
+                        var bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOF>", StringComparison.Ordinal) > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    // Show the data on the console.
+                    Console.WriteLine("Text received : {0}", data);
+
+                    // Echo the data back to the client.
+                    var msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                    Console.WriteLine("Press Enter to exit...");
+                    Console.ReadLine();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+
+        }
+
+        public static int Main(String[] args)
+        {
+            StartListening();
+            return 0;
+        }
+    }
+}
